ca410b173d5acd162f022eb8271923896a7355e1
Nick <nsardonini@gmail.com>
2020-03-26 21:03:57 -0400

    add #my_select, #my_all?, #my_any?

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -17,4 +17,31 @@ module Enumerable
     end
     self
   end
+
+  def my_select
+    results = []
+    self.my_each do |e|
+      results << e if yield(e)
+    end
+    results
+  end
+
+  def my_all?
+    results = []
+    self.my_each do |e|
+      results << e if yield(e) 
+    end
+    return false if self.size == 0
+    results.size == self.size ? true : false
+  end
+
+  def my_any?
+    results = []
+    self.my_each do |e|
+      results << e if yield(e)
+    end
+    results.size > 0 ? true : false
+  end
+
+  
 end
